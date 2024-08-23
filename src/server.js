b76a75330e4e181e2b5a4ac541751f06cab5c8a1
OlenaKononenko23 <148655722+OlenaKononenko23@users.noreply.github.com>
2024-08-23 13:37:50 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -35,21 +35,6 @@ export const startServer = () => {
   // Додаємо роутер
   app.use(contactsRouter);
 
-  // Обробник для неіснуючих маршрутів
-  app.use('*', (req, res) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  // Глобальний обробник помилок
-  app.use((err, req, res) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
-
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
