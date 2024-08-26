c745322bede8f0cb2f65bd616666d576d08abf99
OlenaKononenko23 <148655722+OlenaKononenko23@users.noreply.github.com>
2024-08-26 20:11:36 +0300

    1

--- src/server.js
+++ src/server.js
@@ -1,16 +1,11 @@
 import express from 'express';
-import cors from 'cors';
 import pino from 'pino-http';
-import cookieParser from 'cookie-parser';
-
-import dotenv from 'dotenv';
+import cors from 'cors';
 import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-
 import router from './routers/index.js';
-
-dotenv.config();
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -24,7 +19,6 @@ export const startServer = () => {
     }),
   );
   app.use(cors());
-
   app.use(cookieParser());
 
   app.use(
@@ -35,29 +29,12 @@ export const startServer = () => {
     }),
   );
 
-  // Додаємо роутер
   app.use(router);
-
-  // Обробник для неіснуючих маршрутів
-  app.use('*', (req, res) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  // Глобальний обробник помилок
-  app.use((err, req, res) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
 
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
 
-  // Запуск сервера
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
